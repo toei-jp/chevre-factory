158d7aa68c3e6e504848f74fbc17093a090bd46f
phi.nt <phi.nt@briswell.com>
2018-10-18 17:21:10 +0700

    ### Changed - 配給検索インターフェースを追加

--- src/factory/distributions/distribute.ts
+++ src/factory/distributions/distribute.ts
@@ -7,3 +7,13 @@ export interface IDistributions extends DistributionsFactory.IDistributions {
     id: string;
     name: string;
 }
+
+/**
+ * 検索条件インターフェース
+ */
+export interface ISearchConditions {
+    limit?: number;
+    page?: number;
+    name?: string;
+    id?: string;
+}
