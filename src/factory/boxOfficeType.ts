dda1191fe45ba42ab87fca5116fbe78eaf74c7ee
thanhBriswell <thanh.clg@briswell.com>
2018-10-19 10:41:52 +0700

    ###added 興行区分インターフェースを追加

--- src/factory/boxOfficeType.ts
+++ src/factory/boxOfficeType.ts
@@ -1,7 +1,14 @@
-import IMultilingualString from './multilingualString';
-
 export interface IBoxOfficeTypeAttributes {
-    name: IMultilingualString;
+    name: string;
+}
+/**
+ * 検索条件インターフェース
+ */
+export interface ISearchConditions {
+    limit?: number;
+    page?: number;
+    name?: string;
+    id?: string;
 }
 /**
  * 興行区分インターフェース
